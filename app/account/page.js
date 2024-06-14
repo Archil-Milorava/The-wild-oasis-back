29dbfd56df923273526a35df3e50932edfdcbdf3
Archil-Milorava <achimilorava16@gmail.com>
2024-06-14 21:53:45 +0400

    almost finished

---
+++ app/account/page.js
@@ -0,0 +1,15 @@
+
+export const metadata = {
+    title: 'Guest area'
+}
+
+
+function page() {
+    return (
+        <h2 className="font-semibols text-2xl text-accent-400 mb-7">
+Welcome, Jonas
+        </h2>
+    )
+}
+
+export default page
